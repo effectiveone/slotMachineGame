2ac9c3b5773d7b6fa4fd29a9e706c03a4653e622
Konrad <85121335+effectiveone@users.noreply.github.com>
2023-02-19 12:18:44 +0100

    add wallet

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-const slotItems = ["🍒", "🍇", "🍉", "🍊", "🍓", "🍋", "🍍", "🍑"];
+const slotItems = [
+  { symbol: "🍒", points: 10 },
+  { symbol: "🍇", points: 20 },
+  { symbol: "🍉", points: 30 },
+  { symbol: "🍊", points: 40 },
+  { symbol: "🍓", points: 50 },
+  { symbol: "🍋", points: 60 },
+  { symbol: "🍍", points: 70 },
+  { symbol: "🍑", points: 80 },
+];
 
 export default function App() {
-  const [slot1, setSlot1] = useState("🍒");
-  const [slot2, setSlot2] = useState("🍒");
-  const [slot3, setSlot3] = useState("🍒");
+  const [slot1, setSlot1] = useState(slotItems[0]);
+  const [slot2, setSlot2] = useState(slotItems[0]);
+  const [slot3, setSlot3] = useState(slotItems[0]);
+  const [wallet, setWallet] = useState(0);
 
   const spin = () => {
     // Losuj trzy symbole
@@ -18,16 +28,22 @@ export default function App() {
     setSlot1(item1);
     setSlot2(item2);
     setSlot3(item3);
+
+    // Dodaj nagrodę punktową do portfela, jeśli wylosowano pasujące symbole
+    if (item1.symbol === item2.symbol && item2.symbol === item3.symbol) {
+      setWallet(wallet + item1.points);
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Slot Machine</Text>
       <View style={styles.slotsContainer}>
-        <Text style={styles.slot}>{slot1}</Text>
-        <Text style={styles.slot}>{slot2}</Text>
-        <Text style={styles.slot}>{slot3}</Text>
+        <Text style={styles.slot}>{slot1.symbol}</Text>
+        <Text style={styles.slot}>{slot2.symbol}</Text>
+        <Text style={styles.slot}>{slot3.symbol}</Text>
       </View>
+      <Text style={styles.wallet}>Wallet: {wallet}</Text>
       <TouchableOpacity style={styles.button} onPress={spin}>
         <Text style={styles.buttonText}>Spin</Text>
       </TouchableOpacity>
@@ -54,6 +70,11 @@ const styles = StyleSheet.create({
   slot: {
     fontSize: 60,
     marginHorizontal: 10,
+  },
+  wallet: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
   button: {
     backgroundColor: "blue",
